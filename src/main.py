ac93f3f45531f56435e8c986bd8d8d767d034cbf
HerrPhi <fingatta@gmail.com>
2025-10-30 16:02:27 +0100

    added markdown to html

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import TextNode, TextType
 
+from markdown_to_html import markdown_to_html_node
+
 def main():
     node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(node)
+
 
 
 if __name__ == "__main__":
